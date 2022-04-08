b6ed9a3a595be3b3e11c6f30b043e6c73431f85d
ArashAmiry <Arashamiry@hotmail.com>
2022-04-08 23:28:19 +0200

    Trying to get the correct time, taking the lecture into consideration

--- global_url.py
+++ global_url.py
@@ -1,4 +1,4 @@
 BASE_URL = "https://api.vasttrafik.se/bin/rest.exe/v2/"
 TOKEN_URL = "https://api.vasttrafik.se/token/"
-AUTH_KEY = <Your Key>
-AUTH_SECRET = <Your Secret>
+AUTH_KEY = "Ji5J7Sb_kPqq5q2UXowfDOsTE50a"
+AUTH_SECRET = "ZQcyi3TrZZ28pEbVp8UlVJoGkVoa"
