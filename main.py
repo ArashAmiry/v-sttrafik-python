7adf91be5f213a763a02fd5dc4762b40ed2f656e
ArashAmiry <Arashamiry@hotmail.com>
2022-04-08 20:32:28 +0200

    Fully working Borås Resecentrum to Chalmers Application

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from global_url import *
 
 if __name__ == '__main__':
     api = API()
-    response = api.calculate_trip(57.72002774943832, 12.932627629303825, "Borås Resecentrum", 57.68997981917106, 11.972944614985485, "Aschebergsgatan", "2022-04-09", "10:00")
-    print(response["TripList"]["Trip"]["Leg"][0]["Origin"]["time"])
-    pass
+    print("What date do you need to know?")
+    date = input("Format: YYYY-MM-DD\n")
+    print("When does Nils start his first lesson: ")
+    time = input("Format: HH:MM\n")
+    response = api.calculate_trip(57.72002774943832, 12.932627629303825, "Borås Resecentrum", 57.68997981917106, 11.972944614985485, "Chalmers", date, time)
+    print(response["TripList"]["Trip"]["Leg"][0]["Destination"]["time"])
+
 
 
